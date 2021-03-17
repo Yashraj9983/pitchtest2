10419d61725c6b9f4496a60bedad3e67cbb26477
Yashraj9983 <67761589+Yashraj9983@users.noreply.github.com>
2021-03-17 16:37:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+const model_url='https://cdn.jsdelivr.net/gh/Yashraj9983/pitchtest2/crepe/'
 let pitch;
 let audioContext;
 let mic;
@@ -55,7 +56,7 @@ function listening(){
   audioContext.resume();
 
   pitch=ml5.pitchDetection(
-   'crepe/model.json',
+   model_url,
   audioContext,
   mic.stream,
   modelLoaded);
